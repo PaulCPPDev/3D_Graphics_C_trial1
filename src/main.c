142a77f6a267b83517260a840d2d0c1295784988
PaulCPPDev <holderbaumpaul@gmail.com>
2023-12-15 21:18:39 +0100

    perspective projection top view, no camera

--- src/main.c
+++ src/main.c
@@ -9,6 +9,8 @@
 vec3_t cube_points[N_POINTS];
 vec2_t  projected_points[N_POINTS];
 
+vec3_t camera_position = {.x= 0, .y = 0, .z = -5};
+
 float fov_factor = 128;
 
 bool is_running = false;
@@ -59,8 +61,8 @@ void process_input(void){
 
 vec2_t project(vec3_t point){
 	vec2_t projected_point = {
-		.x = (fov_factor * point.x),
-		.y = (fov_factor * point.y)
+		.x = (fov_factor * point.x)/ point.z,
+		.y = (fov_factor * point.y)/ point.z
 	};
 	return projected_point;
 }
