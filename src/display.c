291822972abd6f6f4dac4ab368ef9b495e368fbd
PaulCPPDev <holderbaumpaul@gmail.com>
2023-12-15 20:24:34 +0100

    orthographic projection

--- src/display.c
+++ src/display.c
@@ -58,10 +58,33 @@ bool initialize_window(void){
 }
 
 
+
+void draw_grid(void) {
+    for (int y = 0; y < window_height; y += 10) {
+        for (int x = 0; x < window_width; x += 10) {
+            color_buffer[(window_width * y) + x] = 0xFF444444;
+        }
+    }
+}
+
+
+
 void draw_pixel(int x, int y, uint32_t color){
-	if (x < window_width && y < window_height)
+	if (x >= 0 && x < window_width && y>=0 && y < window_height)
 		color_buffer[ (window_width * y) + x ] = color;
 }
+
+
+void draw_rect(int x, int y, int width, int height, uint32_t color) {
+    for (int i = 0; i < width; i++) {
+        for (int j = 0; j < height; j++) {
+            int current_x = x + i;
+            int current_y = y + j;
+            draw_pixel(current_x, current_y, color);
+        }
+    }
+}
+
 
 void render_color_buffer(void){
 	SDL_UpdateTexture(
--- src/main.c
+++ src/main.c
@@ -5,6 +5,11 @@
 #include "display.h"
 #include "vector.h"
 
+#define N_POINTS (9 * 9 * 9) // 9x9x9 cube
+vec3_t cube_points[N_POINTS];
+vec2_t  projected_points[N_POINTS];
+
+float fov_factor = 128;
 
 bool is_running = false;
 
@@ -20,6 +25,21 @@ void setup(void){
 			window_width,
 			window_height
 			);
+	
+	int point_count = 0;
+
+	// Start loading the array of vectors
+	// from -1 to 1 ( in this 9X9X9 cube)
+	
+	for(float x = -1; x <= 1; x += 0.25){
+		for (float y = -1 ; y <=1; y += 0.25){
+			for(float z = -1; z <=1; z += 0.25){
+				vec3_t new_point = {.x = x, .y =y, .z= z};
+				cube_points[point_count] = new_point;
+				point_count++;
+			}
+		}
+	}
 }
 
 void process_input(void){
@@ -37,17 +57,42 @@ void process_input(void){
 	}
 }
 
+vec2_t project(vec3_t point){
+	vec2_t projected_point = {
+		.x = (fov_factor * point.x),
+		.y = (fov_factor * point.y)
+	};
+	return projected_point;
+}
+
 void update(void){
+
+	for(int i = 0; i <N_POINTS; i++) {
+		// project each point in the array
+		vec2_t projected_point = project(cube_points[i]);
+
+		// add all the projected points to the array of projected points
+		projected_points[i] = projected_point;
+	}
 
 }
 
 
 void render(void){
-	SDL_SetRenderDrawColor(renderer, 255, 0,0, 255);
-	SDL_RenderClear(renderer);
+	
+	draw_grid();
 
-
-	draw_pixel(20, 20, 0xFFFFFF00);
+	// Loop all projected points and render them
+	for( int i= 0; i < N_POINTS; i++ ){
+		vec2_t projected_point = projected_points[i];
+		draw_rect(
+			projected_point.x + (window_width / 2),
+			projected_point.y + (window_height / 2),
+			4,
+			4,
+			0xFFFFFF00
+				);
+	}
 
 	render_color_buffer();
 	clear_color_buffer(0xFF000000);
@@ -66,7 +111,6 @@ int main (){
 
 	setup();
 
-	vec3_t myvector = { 2.0, 3.0, -4.0};
 
 	while(is_running){
 		process_input();
