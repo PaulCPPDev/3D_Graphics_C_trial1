9e47eab356d492346ad06e8b3a976a1437a9fba2
PaulCPPDev <holderbaumpaul@gmail.com>
2023-12-27 17:11:24 +0100

    Matrix Scaling

--- src/mesh.c
+++ src/mesh.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-#include "mesh.h"
-#include "array.h"
 #include <string.h>
+#include "array.h"
+#include "mesh.h"
 
 mesh_t mesh = {
-	.vertices = NULL,
-	.faces = NULL,
-	.rotation = {0, 0, 0}
+    .vertices = NULL,
+    .faces = NULL,
+    .rotation = { 0, 0, 0 },
+    .scale = { 1.0, 1.0, 1.0 },
+    .translation = { 0, 0, 0 }
 };
 
 vec3_t cube_vertices[N_CUBE_VERTICES] = {
@@ -22,38 +24,35 @@ vec3_t cube_vertices[N_CUBE_VERTICES] = {
 
 face_t cube_faces[N_CUBE_FACES] = {
     // front
-    { .a = 1, .b = 2, .c = 3 },
-    { .a = 1, .b = 3, .c = 4 },
+    { .a = 1, .b = 2, .c = 3, .color = 0xFFFF0000 },
+    { .a = 1, .b = 3, .c = 4, .color = 0xFFFF0000 },
     // right
-    { .a = 4, .b = 3, .c = 5 },
-    { .a = 4, .b = 5, .c = 6 },
+    { .a = 4, .b = 3, .c = 5, .color = 0xFF00FF00 },
+    { .a = 4, .b = 5, .c = 6, .color = 0xFF00FF00 },
     // back
-    { .a = 6, .b = 5, .c = 7 },
-    { .a = 6, .b = 7, .c = 8 },
+    { .a = 6, .b = 5, .c = 7, .color = 0xFF0000FF },
+    { .a = 6, .b = 7, .c = 8, .color = 0xFF0000FF },
     // left
-    { .a = 8, .b = 7, .c = 2 },
-    { .a = 8, .b = 2, .c = 1 },
+    { .a = 8, .b = 7, .c = 2, .color = 0xFFFFFF00 },
+    { .a = 8, .b = 2, .c = 1, .color = 0xFFFFFF00 },
     // top
-    { .a = 2, .b = 7, .c = 5 },
-    { .a = 2, .b = 5, .c = 3 },
+    { .a = 2, .b = 7, .c = 5, .color = 0xFFFF00FF },
+    { .a = 2, .b = 5, .c = 3, .color = 0xFFFF00FF },
     // bottom
-    { .a = 6, .b = 8, .c = 1 },
-    { .a = 6, .b = 1, .c = 4 }
+    { .a = 6, .b = 8, .c = 1, .color = 0xFF00FFFF },
+    { .a = 6, .b = 1, .c = 4, .color = 0xFF00FFFF }
 };
 
 void load_cube_mesh_data(void) {
-	for(int i = 0; i < N_CUBE_VERTICES; i++){
-		vec3_t cube_vertex = cube_vertices[i];
-		array_push(mesh.vertices, cube_vertex);
-	}
-
-	for (int i = 0; i < N_CUBE_FACES; i++){
-		face_t cube_face  = cube_faces[i];
-		array_push(mesh.faces, cube_face);
-	}
+    for (int i = 0; i < N_CUBE_VERTICES; i++) {
+        vec3_t cube_vertex = cube_vertices[i];
+        array_push(mesh.vertices, cube_vertex);
+    }
+    for (int i = 0; i < N_CUBE_FACES; i++) {
+        face_t cube_face = cube_faces[i];
+        array_push(mesh.faces, cube_face);
+    }
 }
-
-
 
 void load_obj_file_data(char* filename) {
     FILE* file;
@@ -68,7 +67,7 @@ void load_obj_file_data(char* filename) {
             array_push(mesh.vertices, vertex);
         }
         // Face information
-        if (strncmp(line, "f ", 2) == 0){
+        if (strncmp(line, "f ", 2) == 0) {
             int vertex_indices[3];
             int texture_indices[3];
             int normal_indices[3];
@@ -84,7 +83,6 @@ void load_obj_file_data(char* filename) {
                 .c = vertex_indices[2]
             };
             array_push(mesh.faces, face);
-       		}
-	}
+        }
+    }
 }
-
--- src/vector.c
+++ src/vector.c
@@ -2,7 +2,7 @@
 #include "vector.h"
 
 ///////////////////////////////////////////////////////////////////////////////
-// Implementations of Vector 2D functions
+// Implementations of Vector 2 functions
 ///////////////////////////////////////////////////////////////////////////////
 float vec2_length(vec2_t v) {
     return sqrt(v.x * v.x + v.y * v.y);
@@ -51,7 +51,7 @@ void vec2_normalize(vec2_t* v) {
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-// Implementations of Vector 3D functions
+// Implementations of Vector 3 functions
 ///////////////////////////////////////////////////////////////////////////////
 float vec3_length(vec3_t v) {
     return sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
@@ -112,7 +112,6 @@ void vec3_normalize(vec3_t* v) {
     v->y /= length;
     v->z /= length;
 }
-
 vec3_t vec3_rotate_x(vec3_t v, float angle) {
     vec3_t rotated_vector = {
         .x = v.x,
@@ -138,4 +137,17 @@ vec3_t vec3_rotate_z(vec3_t v, float angle) {
         .z = v.z
     };
     return rotated_vector;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Implementations of Vector conversion functions
+///////////////////////////////////////////////////////////////////////////////
+vec4_t vec4_from_vec3(vec3_t v) {
+    vec4_t result = { v.x, v.y, v.z, 1.0 };
+    return result;
+}
+
+vec3_t vec3_from_vec4(vec4_t v) {
+    vec3_t result = { v.x, v.y, v.z };
+    return result;
 }
